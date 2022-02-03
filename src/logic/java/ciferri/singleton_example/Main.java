43f425595b68577fd6ea254ef2b1e54b629e80f2
SimoneCiferri <simoneciferri0112@gmail.com>
2022-02-03 10:53:22 +0100

    LOC

--- src/logic/java/ciferri/singleton_example/Main.java
+++ src/logic/java/ciferri/singleton_example/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 450, 300);
+        Scene scene = new Scene(fxmlLoader.load(), 750, 400);
         stage.setTitle("Singleton Example!");
         stage.setScene(scene);
         stage.show();
--- src/logic/java/ciferri/singleton_example/view_controller/ViewController.java
+++ src/logic/java/ciferri/singleton_example/view_controller/ViewController.java
@@ -12,7 +12,13 @@ public class ViewController {
     private Label lblToken;
 
     @FXML
+    private Label lblToken2;
+
+    @FXML
     private TextField tfInput;
+
+    @FXML
+    private TextField tfInput2;
 
     @FXML
     private void generatePersonalToken(){
@@ -20,6 +26,15 @@ public class ViewController {
             SingletonClass singleton = SingletonClass.getSingletonInstance();
             String token = singleton.getMyToken(tfInput.getText());
             lblToken.setText(token);
+        }
+    }
+
+    @FXML
+    private void generatePersonalToken2(){
+        if(!Objects.equals(tfInput2.getText(), "")){
+            SingletonClass singleton2 = SingletonClass.getSingletonInstance();
+            String token2 = singleton2.getMyToken(tfInput2.getText());
+            lblToken2.setText(token2);
         }
     }
 }
